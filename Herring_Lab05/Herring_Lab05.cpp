1f4b413cefd1cf2647cb783dc70d2d867a0eb1f5
herrinb <benjaminherring27@gmail.com>
2024-09-25 12:58:03 -0400

    final

--- Herring_Lab05/Herring_Lab05.cpp
+++ Herring_Lab05/Herring_Lab05.cpp
@@ -1,20 +1,38 @@
-// Herring_Lab05.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <vector>
+#include <string>
 
+using namespace std;
+
+
+char cypher(char letter, vector<char> cypher) {
+    int current = (int)letter;
+    int temp;
+    if (current >= 65 && current <= 90) {
+        return cypher[current - 65];
+    }else if (current >= 97 && current <= 122) {
+        temp = current - 32;
+        current = temp - 65;
+        letter = cypher[current];
+        current = ((int)letter) + 32;
+        return (char)current;
+        
+        
+    }else {
+        return letter;
+    }
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+    vector<char> stringVector{ 'V','F','X','B','L','I','T','Z','J','R','P','H','D','K','N','O','W','S','G','U','Y','Q','M','A','C','E' };
+    string(text);
+    cout << "Input text to cypher: ";
+    getline(cin, text);
+    int i = 0;
+    while (i < (text.length())) {
+        text.at(i) = cypher(text.at(i), stringVector);
+        i++;
+    }
+    cout << "Encoded Message: " << text;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
